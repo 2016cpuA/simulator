5bdd3bf6a05160352b17c4ac9f0f323442a25d27
zukki259 <yuya.su@outlook.jp>
2016-10-04 01:05:17 +0900

    Update instructs.h

--- instructs.h
+++ instructs.h
@@ -4,21 +4,26 @@
 #define INSTR_H
 
 /* ここに命令セット */
-/*opcode記述箇所*/
+/*opcode+function記述箇所*/
 #define NOP 0
-#define ADD 1
-#define ADDI 2
-#define SUB 3
-#define MUL 4
-#define MULI 5
-#define DIV 6
-#define STR 16
-#define STRI 17
-#define LOAD 20
-#define LOADI 21
+#define ADD 0x20
+#define ADDI 0x2000003F
+#define SUB 0x1A
+#define MULT 0x18
+#define DIV 0x22
+#define SLT 0x2A
+#define BEQ 0x1000003F
+#define BNE 0x1400003F
+#define J 0x0800003F
+#define JAL 0x0C00003F
+#define JR 8
+#define LW 0x8C00003F
+#define SW 0xAC00003F
+#define AND 0x24
+
 #define INSTR_END -2147483648
 
-/*オペランド指定用;今のところreadline.cのみで使用*/
+/*オペランド指定用;今のところreadline.cのみで使用;構文チェック用*/
 #define UNKNOWN -1
 #define ERROR -2
 #define REG 1
@@ -26,7 +31,7 @@
 #define IMMIDIATE 4
 #define NONE 0
 
-/*命令実行用の関数のプロトタイプ宣言
+/*命令実行用の関数のプロトタイプ宣言(まだ仮置きのまま)
   実体はinstructs.cに記述*/
 int instr_nop(Simulator *sim);
 /*pc <- pc+1*/
