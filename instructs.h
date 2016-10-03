67c9fe0d389167845e9d3f3929b34296dfa0e34a
zukki259 <yuya.su@outlook.jp>
2016-10-03 18:20:50 +0900

    Update instructs.h
    各関数・宣言の説明を記載

--- instructs.h
+++ instructs.h
@@ -3,6 +3,8 @@
 
 #define INSTR_H
 
+/* ここに命令セット */
+/*opcode記述箇所*/
 #define NOP 0
 #define ADD 1
 #define ADDI 2
@@ -15,31 +17,37 @@
 #define LOAD 20
 #define LOADI 21
 #define INSTR_END -2147483648
-int op_nop(Simulator *sim);
+
+/*命令実行用の関数のプロトタイプ宣言
+  実体はinstructs.cに記述*/
+int instr_nop(Simulator *sim);
 /*pc <- pc+1*/
-int op_add(Simulator *sim,int ra,int rb,int rc);
+int instr_add(Simulator *sim,int ra,int rb,int rc);
 /*Ra <- Rb+Rc; pc<-pc+1*/
-int op_addi(Simulator *sim,int ra,int rb,int i);
+int instr_addi(Simulator *sim,int ra,int rb,int i);
 /*Ra <- Rb+i; pc<-pc+1*/
-int op_sub(Simulator *sim,int ra,int rb,int rc);
+int instr_sub(Simulator *sim,int ra,int rb,int rc);
 /*Ra <- Rb-Rc; pc<-pc+1*/
-int op_mul(Simulator *sim,int ra,int rb,int rc);
+int instr_mul(Simulator *sim,int ra,int rb,int rc);
 /*Ra <- Rb*Rc; pc<-pc+1*/
-int op_muli(Simulator *sim,int ra,int rb,int i);
+int instr_muli(Simulator *sim,int ra,int rb,int i);
 /*Ra <- Rb*i; pc<-pc+1*/
-int op_div(Simulator *sim,int ra,int rb,int rc);
+int instr_div(Simulator *sim,int ra,int rb,int rc);
 /*Ra <- Rb/Rc; pc<-pc+1*/
-int op_store(Simulator *sim,int rs,int addr);
+int instr_store(Simulator *sim,int rs,int addr);
 /*mem[addr] <- rs; pc<-pc+1;*/
-int op_store_indir(Simulator *sim,int rs, int raddr);
+int instr_store_indir(Simulator *sim,int rs, int raddr);
 /*mem[Raddr] <- rs; pc<-pc+1*/
-int op_load(Simulator *sim,int rd,int addr);
+int instr_load(Simulator *sim,int rd,int addr);
 /*Rd <- mem[addr]; pc<-pc+1*/
-int op_load_indir(Simulator *sim,int rd,int raddr);
+int instr_load_indir(Simulator *sim,int rd,int raddr);
 /*Rd <- mem[Raddr]; pc<-pc+1*/
 
-int interpret_instr(Instruct* instr,char *name,char *op1,char *op2,char *op3,char *op4);
+/*命令名を読み取るための関数
+  実体はinstructs.cに記述*/
+int get_opcode(char *name);
 
+/*その命令で行う操作を記述する関数*/
 int execute_instr(Simulator* sim,Instruct instr);
 
 #endif
